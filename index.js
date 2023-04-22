dfa0b26f4cca3169b9513077c1279004e96c758b
yuvalpress <yuvalpress@gmail.com>
2023-04-23 01:47:32 +0300

    fix css

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
         placeholder: l('search-placeholder'),
         noData: l('search-noData'),
       },
-      plugins: [pluginChapterNav, zoomImg, analytics],
+      plugins: [pluginChapterNav, zoomImg],
       copyCode: {
         buttonText : 'Copy',
         errorText  : 'Error',
@@ -62,24 +62,5 @@
   
     function l(key) {
       return document.querySelector(`#strings [${key}]`).textContent
-    }
-  
-    function analytics(hook, vm) {
-      // If stay on any page for more than 30s, we count it as a conversion
-      let id 
-      hook.doneEach(() => {
-        clearTimeout(id)
-        id = setTimeout(() => {
-          gtag('event', 'conversion', { path: vm.route.path })
-        }, 30*1000);
-      })
-    
-      hook.mounted(function() {
-        ;(function (c, l, a, r, i, t, y) {
-          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-          y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-        })(window, document, "clarity", "script", "b4efllwpue");
-      })
     }
   })()
